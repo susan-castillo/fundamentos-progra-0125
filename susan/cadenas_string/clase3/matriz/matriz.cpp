e2a844f3b48925eaeeee4e74655b4c7b90c76966
EmelyFuentes <00126025Quca.edu.sv>
2025-06-11 12:28:19 -0600

    Trabajo de matrices

--- susan/cadenas_string/clase3/matriz/matriz.cpp
+++ susan/cadenas_string/clase3/matriz/matriz.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int mat[50][50];
-    int i, j;
-    int cols, rows;
+int main() {
+  int mat[50][50];
+  int sumas[50];
+  int i, j;
+  int cols, rows;
+  /*     int acum; */
 
-    cout << "LLENAR UNA MATRIZ" << endl;
-    cout << "Cuantas filas tendra: ";
-    cin >> rows;
+  cout << "LLENAR UNA MATRIZ" << endl;
+  cout << "Cuantas filas tendra: ";
+  cin >> rows;
 
-    cout << "Cuantas columnas tendra: ";
-    cin >> cols;
+  cout << "Cuantas columnas tendra: ";
+  cin >> cols;
 
-    cout << "Digite los datos de la matriz: " << endl;
-    for(i =0; i < rows; i++){
-        for(j = 0; j <cols; j++){
-            cout << "mat[" << i << ", " << j << "]: ";
-            cin >> mat[i][j];
-        }
+  cout << "Digite los datos de la matriz: " << endl;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      cout << "mat[" << i << ", " << j << "]: ";
+      cin >> mat[i][j];
     }
+  }
 
+  cout << "Los datos de la matriz son: " << endl;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      cout << mat[i][j] << " ";
+    }
     cout << endl;
-    cout << "Los datos de la matriz son: " << endl;
-    for(i =0; i < rows; i++){
-        for(j = 0; j <cols; j++){
-            cout <<  mat[i][j] << " ";
-        }
-        cout << endl;
-    }
+  }
 
-    
+  /*   acum = 0; */
+  for (i = 0; i < rows; i++) {
+    sumas[i] = 0;
+    for (j = 0; j < cols; j++) {
+      /* acum = acum + mat[i][j]; */
+      sumas[i] = sumas[i] + mat[i][j];
+    }
+  }
+
+  cout << "Las sumas de cada fila son: " << endl;
+  for (i = 0; i < rows; i++) {
+    cout << sumas[i] << endl;
+  }
+
+  /* cout << "La suma es: " << acum ; */
 }
---
+++ susan/cadenas_string/clase3/matriz/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+cout << "La suma es: " << acum ;
